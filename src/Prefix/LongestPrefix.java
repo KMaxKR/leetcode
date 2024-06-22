a09c7bb33d755c9feabc63a7d41fbc2e5ae26124
KiRa <cresciucmax@gmail.com>
2024-06-22 10:57:18 +0300

    main

--- src/Prefix/LongestPrefix.java
+++ src/Prefix/LongestPrefix.java
@@ -1,19 +1,30 @@
 package Prefix;
 
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
 public class LongestPrefix {
     public String longestCommonPrefix(String[] strs) {
         String output = "";
+        LinkedList<Character> list = new LinkedList<>(){};
         for (int i = 0; i < strs.length - 1; i++){
             char[] prev = strs[i].toCharArray();
             char[] next = strs[i + 1].toCharArray();
             for (int j = 0; j < Math.min(prev.length, next.length); j++){
-                if (prev[j] == next[j] && !output.contains(String.valueOf(prev[j]))){
-                    output += prev[j];
+                if (prev[j] == next[j]){
+                    list.add(prev[j]);
                 }
             }
         }
-
-        return output.toString();
+        for (int i = 0; i < list.size(); i++){
+            for (int j = i + 1; j < list.size(); j++){
+                if (list.get(i).equals(list.get(j))){
+                    output += list.get(i);
+                }
+            }
+        }
+        return output;
     }
 }
