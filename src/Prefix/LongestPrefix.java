223ee33d3a75c75a41da7c1270f463526405d5d2
KiRa <cresciucmax@gmail.com>
2024-06-22 11:18:19 +0300

    main

--- src/Prefix/LongestPrefix.java
+++ src/Prefix/LongestPrefix.java
@@ -1,10 +1,6 @@
 package Prefix;
 
-
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Set;
 
 public class LongestPrefix {
     public String longestCommonPrefix(String[] strs) {
