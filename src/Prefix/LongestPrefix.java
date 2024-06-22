72f0dc7690cde1b6592d87b75e914536eec60278
KiRa <cresciucmax@gmail.com>
2024-06-22 10:46:52 +0300

    main

--- src/Prefix/LongestPrefix.java
+++ src/Prefix/LongestPrefix.java
@@ -1,30 +1,19 @@
 package Prefix;
 
-import java.util.HashSet;
-import java.util.Set;
 
 public class LongestPrefix {
     public String longestCommonPrefix(String[] strs) {
-        char[] f_el = strs[0].toCharArray();
-        StringBuilder output = new StringBuilder();
-        int count = 0;
-        for (int i = 1; i < strs.length; i++) {
-            char[] el = strs[i].toCharArray();
-            count = Math.min(Math.min(f_el.length, el.length), strs.length);
-            for (int j = 0; j < count; j++) {
-                char[] r = strs[j].toCharArray();
-                if (f_el[j] == r[j]) {
-                    output.append(f_el[j]);
-                } else {
-                    break;
+        String output = "";
+        for (int i = 0; i < strs.length - 1; i++){
+            char[] prev = strs[i].toCharArray();
+            char[] next = strs[i + 1].toCharArray();
+            for (int j = 0; j < Math.min(prev.length, next.length); j++){
+                if (prev[j] == next[j] && !output.contains(String.valueOf(prev[j]))){
+                    output += prev[j];
                 }
             }
         }
-        HashSet<Character> out = new HashSet<>();
-        for (char ch : output.toString().toCharArray()){
-            out.add(ch);
-        }
-        output = new StringBuilder(out.toString());
+
         return output.toString();
     }
 }
